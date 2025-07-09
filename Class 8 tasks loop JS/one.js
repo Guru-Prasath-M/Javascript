f37907022d53472625e4281bf5b5c6f6af499b68
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-07-09 10:06:10 +0530

    Loops using for,while,do-while

---
+++ Class 8 tasks loop JS/one.js
@@ -0,0 +1,19 @@
+//Printing using for loop
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+//Printing using while loop
+let i=1;
+while(i<=10){
+    console.log(i)
+    i++;
+}
+
+//Printing using do-while loop
+ let n=1;
+do{
+    console.log(n)
+    n++
+}while(n<=10)
+
---
+++ Class 8 tasks loop JS/task.js
@@ -0,0 +1,11 @@
+// for(let i=1;i<=10;i++){
+//     console.log("7 *",i ,"=",7*i)
+// }
+
+
+//USING ES6 :
+
+for(let i=1;i<=10;i++){
+    //console.log("7*",i,"=",7*i)
+    console.log(`7*${i}=${7*i}`)
+}
