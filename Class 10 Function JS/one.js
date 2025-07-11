38625720f591452a4322ce8d9778f10d4410373e
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-07-11 10:05:47 +0530

    Function & alert

---
+++ Class 10 Function JS/one.js
@@ -0,0 +1,6 @@
+function add(a,b){
+    console.log(a+b)
+}
+add(10,20)  //30
+add(100,"Guru")  //100Guru
+add(100)  //NaN
---
+++ Class 10 Function JS/two.js
@@ -0,0 +1,5 @@
+function add(a,b,c=1){
+    console.log(a+b+c)
+}
+add(1,2,3)
+add(1,2) //if we not declare a value it will take the default value
