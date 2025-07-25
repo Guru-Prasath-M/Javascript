4f560a9ca958a7ce479b016718fa5a877718e395
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-07-25 11:13:17 +0530

    Filter Method

---
+++ Class 19 Clock/filter.js
@@ -0,0 +1,10 @@
+let num = [18,31,44,14,99,4]
+let even_nums = num.filter((num)=>{
+    return num%2 === 0;
+})
+
+// let even_num = num.filter((num)=>num%2 === 0) // Single line fat arrow function
+console.log(num)
+console.log(even_num)
+
+
---
+++ Class 19 Clock/inbuilt.js
@@ -0,0 +1,11 @@
+//It will repeate every 2 sec
+
+setInterval(()=>{
+    console.log("Good Morning")
+},2000) 
+
+
+//It will give only one time
+setTimeout(()=>{
+    console.log("GOOD AFTERNOON")
+},1000)
